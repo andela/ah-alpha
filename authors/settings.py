a40330365b23b9393b63f3f152a1b48c109c52cc
tesh254 <ewachira254@gmail.com>
2019-01-11 04:43:34 +0300

    chore(host on heroku) - Create procfile for heroku config - Configure django-heroku for static files serving
    [Delivers #162948904]

--- authors/settings.py
+++ authors/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1.4/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,3 +147,6 @@ REST_FRAMEWORK = {
         # 'authors.apps.authentication.backends.JWTAuthentication',
     ),
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
