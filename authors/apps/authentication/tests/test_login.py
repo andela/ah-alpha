a4426431e073209a1139213e79dfdcad6e96f93e
james <ke-c02s7kf6fvh3@KE-C02S7KF6FVH3s-MacBook-Pro.local>
2019-01-16 10:19:52 +0300

    feature(validation): Recieve descritive validation error message
    - Users should recieve a descriptive error message on registrattion and login
    
    [Delivers #162948908]

--- authors/apps/authentication/tests/test_login.py
+++ authors/apps/authentication/tests/test_login.py
@@ -4,39 +4,120 @@ from rest_framework.views import status
 
 class UserLoginTest(APITestCase):
     """
-        Holds test for handling login
+            Holds test for handling login
     """
     def setUp(self):
         """Setup method"""
         self.client = APIClient()
         self.registration_uri = reverse('auth:register')
         self.login_uri = reverse('auth:login')
-
-        # Json data
-        self.valid_login_data = {
-            "user":{
-                "email":"ewachira254@gmail.com",
-                "password":"@Wachira254"
-            }
-        }
         self.valid_user_credentials = {
             "user": {
                 "username":"Wachira",
-                "email":"ewachira254@gmail.com",
+                "email":"ewwachira254@gmail.com",
                 "password":"@Wachira254"
             }
         }
+        self.valid_login_data = {
+            "user":{
+                "email":"ewwachira254@gmail.com",
+                "password":"@Wachira254"
+            }
+        }
+        self.pwd_missing_special_char = {
+            "user":{
+                "email":"ewwachira254@gmail.com",
+                "password":"wachira254"
+            }
+        }
+        self.pwd_missing_caps = {
+            "user": {
+                "email":"ewwachira254@gmail.com",
+                "password":"@wachira254"
+            }
+        }
 
-    def test_user_login(self):
-        """Test login a user"""
+        self.pwd_missing_number = {
+            "user": {
+                "email":"ewwachira254@gmail.com",
+                "password":"@wachiratesh"
+            }
+        }
+        self.short_pwd = {
+            "user": {
+                "email":"ewwachira254@gmail.com",
+                "password":"@Wa254"
+            }
+        }
+
+    def test_valid_login(self):
+        """Test login a valid user"""
         self.client.post(
             self.registration_uri,
             self.valid_user_credentials,
             format="json"
         )
+
         response = self.client.post(
             self.login_uri,
             self.valid_login_data,
             format="json"
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_special_char(self):
+        """check for special character in password"""
+        self.client.post(
+            self.registration_uri,
+            self.valid_user_credentials,
+            format="json"
+        )
+
+        response = self.client.post(
+            self.login_uri,
+            self.pwd_missing_special_char,
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_caps(self):
+        """Test for capital letter in password"""
+        self.client.post(
+                self.registration_uri,
+                self.valid_user_credentials,
+                format="json"
+        )
+
+        response = self.client.post(
+                self.login_uri,
+                self.pwd_missing_caps,
+                format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_number(self):
+        """Test atleast a number in password"""
+        self.client.post(
+                self.registration_uri,
+                self.valid_user_credentials,
+                format="json"
+        )
+
+        response = self.client.post(
+                self.login_uri,
+                self.pwd_missing_number,
+                format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_length(self):
+        """Test for atleast 8 characteer password"""
+        self.client.post(
+                self.registration_uri,
+                self.valid_user_credentials,
+                format="json"
+        )
+
+        response = self.client.post(
+                self.login_uri,
+                self.short_pwd,
+                format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
---
+++ authors/apps/authentication/validations.py
@@ -0,0 +1,65 @@
+import re
+from django.contrib.auth import authenticate
+
+from django.core.exceptions import ValidationError
+from .messages import error_msg
+from .models import User
+
+class UserValidation():
+    """
+    This is to validate user input on registration and login and return 
+    descriptive validation error messages
+    """
+
+    def valid_email(self, email=None):
+        """
+        Function to validate the user email on registration
+        """
+        user_qs = User.objects.filter(email=email)
+        if user_qs.exists():
+            raise ValidationError(error_msg['usedemail'])
+        elif re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email) is None:
+            raise ValidationError(error_msg['email_format'])
+        return True
+
+    def valid_username(self, username=None):
+        """
+        Function to validate the username on registration
+        """
+        user_qs = User.objects.filter(username=username)
+        if user_qs.exists():
+            raise ValidationError(error_msg['usedname'])
+        elif len(username) < 3:
+            raise ValidationError(error_msg['shortname'])
+        elif re.search('[A-Za-z]',username) is None:
+            raise ValidationError(error_msg['no_letter'])
+        elif re.match('[A-Za-z]|[0-9]',username) is None:
+            raise ValidationError(error_msg['special_character'])
+        return True
+
+    def valid_password(self, password=None):
+        """
+        Function to validate the user password on registration
+        """
+        if len(password) < 8:
+            raise ValidationError(error_msg['short_pwd']) 
+        elif re.search('[0-9]',password) is None:
+            raise ValidationError(error_msg['number_in_pwd'])
+        elif re.search('[a-z]',password) is None:
+            raise ValidationError(error_msg['letter_in_pwd'])
+        elif re.search('[A-Z]',password) is None:
+            raise ValidationError(error_msg['caps_in_pwd'])
+        return True
+    
+    def valid_login_email(self, email=None):
+        """
+        Function to validate the user email on registration
+        """
+        user_qs = User.objects.filter(email=email)
+        if re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email) is None:
+            raise ValidationError(error_msg['email_format'])
+        elif not user_qs.exists():
+            raise ValidationError(error_msg['unregistered_email'])
+        return True
+    
+    
